3acd62e8c453f59723d8d4d1f16a1eacc98058af
JoaquinChou <1486840262@qq.com>
2021-11-05 22:09:40 +0800

    Update 02封装案例.cpp

--- 13_类和对象/02封装案例.cpp
+++ 13_类和对象/02封装案例.cpp
@@ -3,13 +3,19 @@ using namespace std;
 
 // 设计一个学生类，属性有学号和姓名
 // 可以给姓名和学号赋值，可以显示学生的姓名和学号
+
+// 设计学生类
 class Student
 {
+// 私有权限
 private:
+    // 属性
     string m_Name;
     string m_Id;
 
+// 公有权限
 public:
+    // 行为
     void setName(string name)
     {
         this->m_Name = name;
@@ -30,16 +36,20 @@ public:
 
     void showStudentInfo()
     {
-        cout << "学生姓名：" <<  m_Name << "  " << "学生学号：" << m_Id;
+        cout << "学生姓名：" <<  getName() << "  " << "学生学号：" << getId() << endl;
     }
 };
 
 int main()
 {
     // 通过类来实例化对象（通过类来创建对象）
-    Student s1;
+    Student s1, s2;
     s1.setName("张三");
     s1.setId("2334455");
     s1.showStudentInfo();
+
+    s2.setName("李四");
+    s2.setId("24445");
+    s2.showStudentInfo();
     return 0;
 }
