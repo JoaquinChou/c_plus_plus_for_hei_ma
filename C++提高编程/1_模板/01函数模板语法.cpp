79972e99b1cc9b8ce9df0a869f417e442baec193
JoaquinChou <1486840262@qq.com>
2022-01-30 17:42:44 +0800

    Update 01函数模板语法.cpp

--- C++提高编程/1_模板/01函数模板语法.cpp
+++ C++提高编程/1_模板/01函数模板语法.cpp
@@ -4,14 +4,17 @@ using namespace std;
 // 函数模板
 
 // 交换两个整型的函数
-void swapInt() {
-
-
+void swapInt(int &a, int &b) {
+    int temp = a;
+    a = b;
+    b = temp;
 }
 
 // 交换两个浮点型的函数
-void swapDouble() {
-
+void swapDouble(double &a, double &b) {
+    double temp = a;
+    a = b;
+    b = temp;
 }
 
 // 函数模板
@@ -30,16 +33,18 @@ void test01() {
     double c = 1.1;
     double d = 2.2;
 
+    // swapInt(a, b);
     // 有两种方式使用函数模板
     // 1、自动类型推导：编译器自动推导类型
     mySwap(a, b);
     cout << "a=" << a << endl;
     cout << "b=" << b << endl;
 
+    // swapDouble(c, d);
     // 2、显示指定类型
-    mySwap<int>(a, b);
-    cout << "a=" << a << endl;
-    cout << "b=" << b << endl;
+    mySwap<double>(c, d);
+    cout << "c=" << c << endl;
+    cout << "d=" << d << endl;
 
 }
 
